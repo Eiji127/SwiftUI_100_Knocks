e9e7c2c082dc350f381466e22d95065aaff97e6e
白数 叡司 <eiji-shirakazu@dip-net.co.jp>
2023-09-29 15:22:08 +0900

    [Add]: add Knock4 project.

---
+++ Knock3/Knock3/Knock3App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock3App.swift
+//  Knock3
+//
+//  Created by 白数 叡司 on 2023/09/29.
+//
+
+import SwiftUI
+
+@main
+struct Knock3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
