949e1aa4d8744d6d6776cae23b2b4eb83a775f2a
白数 叡司 <eiji-shirakazu@dip-net.co.jp>
2023-10-05 15:20:52 +0900

    [Add]: add Knock12 project.

---
+++ Knock12/Knock12/Knock12App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock12App.swift
+//  Knock12
+//
+//  Created by 白数 叡司 on 2023/10/05.
+//
+
+import SwiftUI
+
+@main
+struct Knock12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
