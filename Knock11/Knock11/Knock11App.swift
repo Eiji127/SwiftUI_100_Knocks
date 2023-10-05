3388dffdd4c741e1e56a4442203039ba5c26d9f7
白数 叡司 <eiji-shirakazu@dip-net.co.jp>
2023-10-05 14:34:26 +0900

    [Add]: add Knock11 project.

---
+++ Knock11/Knock11/Knock11App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock11App.swift
+//  Knock11
+//
+//  Created by 白数 叡司 on 2023/10/05.
+//
+
+import SwiftUI
+
+@main
+struct Knock11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
