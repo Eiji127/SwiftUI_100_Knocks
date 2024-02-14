fadb6397666a2a822d43e784a8a2a96ac97d6173
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-02-14 10:06:54 +0900

    [Add]: add Knock14 project.

---
+++ Knock14/Knock14/Knock14App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock14App.swift
+//  Knock14
+//
+//  Created by 白数叡司 on 2024/02/14.
+//
+
+import SwiftUI
+
+@main
+struct Knock14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
