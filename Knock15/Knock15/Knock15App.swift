d39edb769b279a372250a2e2ddddfe8997f7bce2
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-02-19 20:32:27 +0900

    [Add]: add Knock15 project of my answer.

---
+++ Knock15/Knock15/Knock15App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock15App.swift
+//  Knock15
+//
+//  Created by 白数叡司 on 2024/02/19.
+//
+
+import SwiftUI
+
+@main
+struct Knock15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
