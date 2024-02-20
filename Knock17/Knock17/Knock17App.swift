9974f129ceba32ca8df69779ac9ad7daa6d25c89
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-02-21 03:12:57 +0900

    [Add]: add Knock17 project of my answer.

---
+++ Knock17/Knock17/Knock17App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock17App.swift
+//  Knock17
+//
+//  Created by 白数叡司 on 2024/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Knock17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
