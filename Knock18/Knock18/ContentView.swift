28b59f89e70f3ef3ef1769604c2b23317aebf236
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-02-21 03:31:45 +0900

    [Add]: add Knock18 project of my answer.

---
+++ Knock18/Knock18/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Knock18
+//
+//  Created by 白数叡司 on 2024/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button(action: {
+                
+            }, label: {
+                Text("Tap Me!!")
+            })
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
