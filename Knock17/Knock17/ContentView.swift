46c55560282e4188bf78d12677a31aabe1898d7d
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-02-21 03:12:57 +0900

    [Add]: add Knock17 project of my answer.

---
+++ Knock17/Knock17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Knock17
+//
+//  Created by 白数叡司 on 2024/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
