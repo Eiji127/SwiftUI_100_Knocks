909518a00d031d44aeb04580a19b2b39fc57b5c0
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-02-21 03:49:16 +0900

    [Add]: add my answer in Knock19.

---
+++ Knock19/Knock19/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Knock19
+//
+//  Created by 白数叡司 on 2024/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped...")
+            Button(action: {
+                
+            }, label: {
+                Text("More")
+            })
+            Text("Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped...")
+            Button(action: {
+                
+            }, label: {
+                Text("More")
+            })
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Knock19/Knock19/Knock19App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock19App.swift
+//  Knock19
+//
+//  Created by 白数叡司 on 2024/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Knock19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
