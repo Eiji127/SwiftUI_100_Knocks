2c650de56c6e3b973633143645859236f82babe1
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-02-21 03:15:04 +0900

    [Update]: fix into correct answer.

--- Knock17/Knock17/ContentView.swift
+++ Knock17/Knock17/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var text: String = ""
+    @State var isAlertShown: Bool = false
+    @State var isPresented: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField(" Input Number", text: $text)
+                .border(Color.gray, width: 0.5)
+            Button {
+                if Int(text) != nil {
+                    isPresented = true
+                } else {
+                    isAlertShown = true
+                }
+            } label: {
+                Text("Show Sheet")
+            }
         }
-        .padding()
+        .alert("Title", isPresented: $isAlertShown) {
+            Button("Back", role: .none) {
+                print("Tapped Alert Button.")
+            }
+        } message: {
+            Text("This is Alert")
+        }
     }
 }
 
