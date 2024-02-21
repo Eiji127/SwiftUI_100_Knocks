579ab223e17d0a6e590b4fe2dbf92449623c970f
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-02-21 21:02:32 +0900

    [Add]: add my answer in Knock20.

---
+++ Knock20/Knock20/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Knock20
+//
+//  Created by 白数叡司 on 2024/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("I ❤️ Snorlax! \nSnorlax is most cute & cool \npowerful Pokemon")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Knock20/Knock20/Knock20App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock20App.swift
+//  Knock20
+//
+//  Created by 白数叡司 on 2024/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Knock20App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
