eab49d405a9b1dc6166d88daa6c1002946cdefcf
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-02-25 19:37:34 +0900

    [Add]: add my answer in Knock22.

---
+++ Knock22/Knock22/Knock22App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock22App.swift
+//  Knock22
+//
+//  Created by 白数叡司 on 2024/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Knock22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
