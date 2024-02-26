9874f9db37bc3a6c55fa2ce0fbc7141a064299db
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-02-26 23:03:01 +0900

    [Add]: add my answer in Knock23.

---
+++ Knock23/Knock23/Knock23App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock23App.swift
+//  Knock23
+//
+//  Created by 白数叡司 on 2024/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Knock23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
