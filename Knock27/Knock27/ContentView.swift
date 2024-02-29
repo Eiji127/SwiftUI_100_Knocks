41aaf99c2d3155264579b9331c7c58ab0841c321
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-03-01 02:29:06 +0900

    [Fix]: fix Knock27.

--- Knock27/Knock27/ContentView.swift
+++ Knock27/Knock27/ContentView.swift
@@ -8,17 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var values = [1 ,2, 3]
+    @State var selectedHour: Int = 0
+    @State var selectedMinute: Int = 30
     
     var body: some View {
-        HStack {
-            Picker("AAA", selection: $values) {
+        GeometryReader { geometry in
+            HStack {
+                Picker(selection: $selectedHour, label: EmptyView()) {
+                    ForEach(0 ..< 24) {
+                        Text("\($0)")
+                    }
+                }
+                .pickerStyle(WheelPickerStyle())
+                .onReceive([selectedHour].publisher.first()) { (value) in
+                    print("hour: \(value)")
+                }
+                .labelsHidden()
+                .frame(width: geometry.size.width / 2, height: geometry.size.height)
+                .clipped()
                 
-            }
-            Picker("AAA", selection: $values) {
-                
+                Picker(selection: $selectedMinute, label: EmptyView()) {
+                    ForEach(0 ..< 60) {
+                        Text("\($0)")
+                    }
+                }
+                .pickerStyle(WheelPickerStyle())
+                .onReceive([selectedMinute].publisher.first()) { (value) in
+                    print("hour: \(value)")
+                }
+                .labelsHidden()
+                .frame(width: geometry.size.width / 2, height: geometry.size.height)
+                .clipped()
             }
         }
+        .padding()
     }
 }
 
