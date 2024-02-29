217ccbee033bea13d3e77436486746ee7acc445f
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-03-01 02:19:55 +0900

    [Add]: add Knock27.

---
+++ Knock27/Knock27/Knock27App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock27App.swift
+//  Knock27
+//
+//  Created by 白数叡司 on 2024/03/01.
+//
+
+import SwiftUI
+
+@main
+struct Knock27App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
