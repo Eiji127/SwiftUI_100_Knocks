2b3daaa0b24e636623440511a2318ab5a64fd103
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-03-21 02:48:43 +0900

    [Add]: add my answer of Knock28.

---
+++ Knock28/Knock28/Knock28App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock28App.swift
+//  Knock28
+//
+//  Created by 白数叡司 on 2024/03/18.
+//
+
+import SwiftUI
+
+@main
+struct Knock28App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
