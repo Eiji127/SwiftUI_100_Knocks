ca7bf90ec52d9b8d181f03424a7801a223856e69
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-03-23 21:42:20 +0900

    [Add]: add my answer in Knock30.

---
+++ Knock30/Knock30/Knock30App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock30App.swift
+//  Knock30
+//
+//  Created by 白数叡司 on 2024/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Knock30App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
