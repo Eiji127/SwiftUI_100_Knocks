33494b27ce7ccf21c65234db1bbe5d3113dd9a4b
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-03-23 22:06:13 +0900

    [Add]: add my answer in Knock31.

---
+++ Knock31/Knock31/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Knock31
+//
+//  Created by 白数叡司 on 2024/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("I Love Snorlax Forward")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Knock31/Knock31/Knock31App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock31App.swift
+//  Knock31
+//
+//  Created by 白数叡司 on 2024/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Knock31App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
