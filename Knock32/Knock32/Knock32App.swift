36def3773b51a81454a9f037e12b2736c3ce602a
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-03-26 12:22:21 +0900

    [Add]: add my answer in Knock32.

---
+++ Knock32/Knock32/Knock32App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock32App.swift
+//  Knock32
+//
+//  Created by 白数叡司 on 2024/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Knock32App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
