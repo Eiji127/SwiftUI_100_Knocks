10cbb739478851cb0aa586999bb8141b6505eb4c
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-03-28 19:35:09 +0900

    [Add]: add my answer in Knock33.

---
+++ Knock33/Knock33/Knock33App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock33App.swift
+//  Knock33
+//
+//  Created by 白数叡司 on 2024/03/28.
+//
+
+import SwiftUI
+
+@main
+struct Knock33App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
