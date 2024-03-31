37fdf22ea29a2bb81228e973c702d35640c915ed
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-04-01 00:01:27 +0900

    [Add]: add my answer of Knock34.

---
+++ Knock34/Knock34/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Knock34
+//
+//  Created by 白数叡司 on 2024/03/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("Pikachu")
+            .resizable()
+            .frame(width: 300, height: 300)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Knock34/Knock34/Knock34App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock34App.swift
+//  Knock34
+//
+//  Created by 白数叡司 on 2024/03/31.
+//
+
+import SwiftUI
+
+@main
+struct Knock34App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
