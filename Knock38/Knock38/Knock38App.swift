7a10a44214e49ad7f1fe9b7817e990fd719d8b83
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-04-05 14:05:09 +0900

    [Add]: add Knock38.

---
+++ Knock38/Knock38/Knock38App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock38App.swift
+//  Knock38
+//
+//  Created by 白数叡司 on 2024/04/05.
+//
+
+import SwiftUI
+
+@main
+struct Knock38App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
