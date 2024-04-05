d8fad4f8b0659e25fe62b9c70622b17d2e419856
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-04-05 15:11:52 +0900

    [Fix]: fix Knock39.

--- Knock39/Knock39/ContentView.swift
+++ Knock39/Knock39/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     let markdownText: String = "**Snorlax** is the **most** cute pokemon!\n[About Snorlax](https://en.wikipedia.org/wiki/Snorlax)"
     
     var body: some View {
-        Text(markdownText)
+        Text(LocalizedStringKey(markdownText))
     }
 }
 
