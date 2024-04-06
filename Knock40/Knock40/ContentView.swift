c86bdf085b4aa125bde9ec02b5ddc6927efaa33e
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-04-06 22:59:41 +0900

    [Add]: add Knock40.

---
+++ Knock40/Knock40/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Knock40
+//
+//  Created by 白数叡司 on 2024/04/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let markdownText: String = "**Snorlax** is the **most** cute pokemon!\n[About Snorlax](https://en.wikipedia.org/wiki/Snorlax)"
+    
+    var body: some View {
+        Text(LocalizedStringKey(markdownText))
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Knock40/Knock40/Knock40App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock40App.swift
+//  Knock40
+//
+//  Created by 白数叡司 on 2024/04/06.
+//
+
+import SwiftUI
+
+@main
+struct Knock40App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
