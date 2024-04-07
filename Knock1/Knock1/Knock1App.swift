8bee69b39b4226f09de05dcb44858aebe10e54d2
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-04-07 21:53:41 +0900

    [Add]: add knock1 project.

---
+++ Knock1/Knock1/Knock1App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock1App.swift
+//  Knock1
+//
+//  Created by 白数 叡司 on 2023/09/29.
+//
+
+import SwiftUI
+
+@main
+struct Knock1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
