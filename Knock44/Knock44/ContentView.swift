9fa7795997baac1993c0b14c8bbb52efd76f6940
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-04-08 22:41:44 +0900

    [Fix]: fix Knock44.

--- Knock44/Knock44/ContentView.swift
+++ Knock44/Knock44/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var text = ""
+    @State var text: String = ""
+    @State var onEditing: Bool = false
     
     var body: some View {
-        TextField("Placeholder", text: $text)
+        VStack {
+            Text(onEditing ? "On Editing" : "Not On Editing")
+            
+            TextField("Placeholder", text: $text, onEditingChanged: { onEditing in
+                print("onEditingChanged: \(onEditing)")
+                self.onEditing = onEditing
+            }, onCommit: {
+                print("onCommit")
+            })
             .textFieldStyle(RoundedBorderTextFieldStyle())
             .padding()
+        }
     }
 }
 
