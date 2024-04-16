082a05649fdf3e13ff23d10f512645463b18218c
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-04-16 21:57:02 +0900

    [Add]: add knock49.

---
+++ Knock49/Knock49/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Knock49
+//
+//  Created by 白数叡司 on 2024/04/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Button {
+            
+        } label: {
+            Text("Play")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Knock49/Knock49/Knock49App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock49App.swift
+//  Knock49
+//
+//  Created by 白数叡司 on 2024/04/16.
+//
+
+import SwiftUI
+
+@main
+struct Knock49App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
