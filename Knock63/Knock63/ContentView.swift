4c8779efd1f70aef3e31d8402533d922f8724e2e
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-05-14 22:01:44 +0900

    [Add]: add Knock63.

---
+++ Knock63/Knock63/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Knock63
+//
+//  Created by 白数叡司 on 2024/05/14.
+//
+
+import SwiftUI
+import MapKit
+
+struct ContentView: View {
+    var body: some View {
+        Map {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Knock63/Knock63/Knock63App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock63App.swift
+//  Knock63
+//
+//  Created by 白数叡司 on 2024/05/14.
+//
+
+import SwiftUI
+
+@main
+struct Knock63App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
