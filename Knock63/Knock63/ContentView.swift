946ad5cbcb315864b153681580fed684e228419b
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-05-15 10:40:16 +0900

    [Fix]: fix Knock63.

--- Knock63/Knock63/ContentView.swift
+++ Knock63/Knock63/ContentView.swift
@@ -7,15 +7,67 @@
 
 import SwiftUI
 import MapKit
+import CoreLocation
 
 struct ContentView: View {
+    @StateObject var viewState: ContentViewState = .init()
+    
     var body: some View {
-        Map {
-            
+        ZStack {
+            if let location = viewState.location {
+                Map(
+                    initialPosition:
+                        MapCameraPosition.region(
+                            MKCoordinateRegion(
+                                center: location,
+                                latitudinalMeters: 300,
+                                longitudinalMeters: 300
+                            )
+                        )
+                )
+            } else {
+                ProgressView()
+            }
+        }
+        .onAppear {
+            viewState.onAppear()
+        }
+    }
+    
+}
+
+class ContentViewState: NSObject, ObservableObject {
+    @Published var location: CLLocationCoordinate2D?
+    private let locationManager = CLLocationManager()
+    
+    func onAppear() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        let status = locationManager.authorizationStatus
+        switch status {
+        case .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+        default:
+            break
         }
     }
 }
 
+extension ContentViewState: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+        default:
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        self.location = manager.location?.coordinate
+    }
+}
+
 #Preview {
-    ContentView()
+    ContentView(viewState: ContentViewState())
 }
