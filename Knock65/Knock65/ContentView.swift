8bfd85dc7c731bac67f617e6e53523a05349a8c2
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-06-02 23:29:49 +0900

    [Add]: add knock65.

---
+++ Knock65/Knock65/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Knock65
+//
+//  Created by 白数叡司 on 2024/06/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Knock65/Knock65/Knock65App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock65App.swift
+//  Knock65
+//
+//  Created by 白数叡司 on 2024/06/02.
+//
+
+import SwiftUI
+
+@main
+struct Knock65App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
