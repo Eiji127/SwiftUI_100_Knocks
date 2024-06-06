2b026eeed7638d1ad4e935bc4d8a031a2862c551
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-06-06 23:30:52 +0900

    [Add]: add knock66.

---
+++ Knock66/Knock66/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Knock66
+//
+//  Created by 白数叡司 on 2024/06/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Knock66/Knock66/Knock66App.swift
@@ -0,0 +1,17 @@
+//
+//  Knock66App.swift
+//  Knock66
+//
+//  Created by 白数叡司 on 2024/06/06.
+//
+
+import SwiftUI
+
+@main
+struct Knock66App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
